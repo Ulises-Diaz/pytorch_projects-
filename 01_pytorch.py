656fa8fa7b01bff409beda670b7c29872a477d95
Ulises-Diaz <A00837223@tec.mx>
2024-12-14 12:22:22 -0600

    1st pytorch model

---
+++ 01_pytorch.py
@@ -0,0 +1,19 @@
+
+#About this notebook
+
+'''
+
+1. : Data (prepare and load),
+2. : Build model ,
+3. : Fitting the model to data
+4. : Saving and loading the model
+5. : Putting it all together 
+
+'''
+import torch 
+from torch import nn #nn contain all of the blocks for Neural Networks 
+import matplotlib.pyplot as plt
+
+print(torch.__version__)
+
+
